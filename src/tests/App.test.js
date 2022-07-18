f2c8dea206098a23c4f4e41cf8bf163ffe22a054
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-07-18 16:25:50 -0300

    req-01-concluido

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Teste os links', () => {
+  test('Teste se o primeiro link possui o texto Home', () => {
+    renderWithRouter(<App />);
+    const linkElement = screen.getByText(/Home/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
+test('Teste se o primeiro link possui o texto About', () => {
+  renderWithRouter(<App />);
+  const linkElement = screen.getByText(/About/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('Teste se o primeiro link possui o texto Favorite Pokémons', () => {
+  renderWithRouter(<App />);
+  const linkElement = screen.getByText(/Favorite/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('Teste se renderiza a pagina notfound', () => {
+  const { history, container } = renderWithRouter(<App />);
+  const notFound = '/NotFound';
+  history.push(notFound);
+  expect(container.innerHTML).toMatch(/Not Found/i);
+});
+
+export default App;
