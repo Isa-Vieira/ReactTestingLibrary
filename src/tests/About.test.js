3fc86bf1c998f916c5c1fbb4b557bbfb503ede2a
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-07-19 17:18:12 -0300

    req-02-concluido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -21,4 +21,11 @@ describe('Teste o componente About', () => {
     const contemParagrafo2 = screen.getByText(/One can filter Pokémons/i);
     expect(contemParagrafo2).toBeInTheDocument();
   });
+  test('Test se a pagina contem a imagem de uma Pokedex', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+    const img = screen.getByRole('img', { name: /pokédex/i });
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste se a mensagem é exibida', () => {
+  test('Teste se a mensagem Page requested not found 😭', () => {
+    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    history.push('/NotFound');
+    const headingH2 = screen.getByRole('heading', {
+      name: /NotFound/i,
+      level: 2,
+
+    });
+    expect(headingH2).toBeInTheDocument();
+  });
+});
