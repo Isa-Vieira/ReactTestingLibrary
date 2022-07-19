1e1d63874c1f10052a7bfa2c1e858ac9886515c4
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-07-19 17:32:31 -0300

    req-04-concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -9,10 +9,17 @@ describe('Teste se a mensagem é exibida', () => {
     const { history } = renderWithRouter(<App />);
     history.push('/NotFound');
     const headingH2 = screen.getByRole('heading', {
-      name: /NotFound/i,
+      name: /Not Found/i,
       level: 2,
-
     });
     expect(headingH2).toBeInTheDocument();
+  });
+  test('Test se a pagina contem a imagem de uma Pokedex', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/NotFoud');
+    const imgMedia = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i });
+    expect(imgMedia).toBeInTheDocument();
+    expect(imgMedia).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
